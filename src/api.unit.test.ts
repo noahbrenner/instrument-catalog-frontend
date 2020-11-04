f70b39791590b263c1a05e8e10c2514e8f9bfd12
Noah Brenner <noah@noahbrenner.com>
2020-11-03 18:31:55 -0800

    Refactor api unit tests to support method params

--- src/api.unit.test.ts
+++ src/api.unit.test.ts
@@ -8,25 +8,29 @@ import {
 import { api } from "#api";
 import type { APIError } from "#api";
 
+const { categories, users } = MOCK_DATA;
+
 type MethodTestSpec = readonly [
   method: keyof typeof api,
   endpoint: keyof typeof ENDPOINTS,
-  dataKey: keyof typeof MOCK_DATA
+  methodArgs: readonly unknown[],
+  expected: unknown
 ];
 
 const HTTP_GET_ENDPOINTS: readonly MethodTestSpec[] = [
-  ["getCategories", "categories", "categories"],
-  ["getUsers", "users", "users"],
+  ["getCategories", "categories", [], { categories }],
+  ["getUsers", "users", [], { users }],
 ];
 
 describe("api", () => {
   describe("given a successful API response", () => {
     test.each(HTTP_GET_ENDPOINTS)(
       '.%s() returns "%s" data',
-      async (method, _endpoint, dataKey) => {
-        const result = await api[method]();
+      async (method, _endpoint, args, expected) => {
+        // @ts-expect-error -- `args` may have different length/types
+        const result = await api[method](...args);
         expect(result).not.toHaveProperty("uiErrorMessage");
-        expect(result.data).toStrictEqual({ [dataKey]: MOCK_DATA[dataKey] });
+        expect(result.data).toStrictEqual(expected);
       }
     );
   });
@@ -34,7 +38,7 @@ describe("api", () => {
   describe("given a network error", () => {
     test.each(HTTP_GET_ENDPOINTS)(
       ".%s() returns a network error message if the error is persistent",
-      (method, endpoint) => {
+      (method, endpoint, args) => {
         server.use(
           rest.get(ENDPOINTS[endpoint], (_req, res) => {
             return res.networkError("Failed to connect");
@@ -43,18 +47,21 @@ describe("api", () => {
 
         expect.assertions(1);
 
-        return (api[method]() as Promise<unknown>).catch((err: APIError) => {
-          expect(err.uiErrorMessage).toStrictEqual(
-            "Couldn't reach the server. Please try reloading in a minute."
-          );
-        });
+        // @ts-expect-error -- `args` may have different length/types
+        return (api[method](...args) as Promise<unknown>).catch(
+          (err: APIError) => {
+            expect(err.uiErrorMessage).toStrictEqual(
+              "Couldn't reach the server. Please try reloading in a minute."
+            );
+          }
+        );
       }
     );
 
     // TODO Enable this test (remove `.skip`) when it's possible to implement it
     test.skip.each(HTTP_GET_ENDPOINTS)(
       ".%s() retries the request",
-      async (method, endpoint, dataKey) => {
+      async (method, endpoint, args, expected) => {
         server.use(
           rest.get(ENDPOINTS[endpoint], (_req, res) => {
             // TODO Return a NetworkError one time (the API doesn't exist yet):
@@ -66,8 +73,9 @@ describe("api", () => {
         expect.assertions(1);
 
         try {
-          const { data } = await api[method]();
-          expect(data).toStrictEqual({ [dataKey]: MOCK_DATA[dataKey] });
+          // @ts-expect-error -- `args` may have different length/types
+          const { data } = await api[method](...args);
+          expect(data).toStrictEqual(expected);
         } catch (err) {
           expect(err).not.toBeDefined();
         }
@@ -78,7 +86,7 @@ describe("api", () => {
   describe("given a 500 status code", () => {
     test.each(HTTP_GET_ENDPOINTS)(
       ".%s() returns a status error message if the error is persistent",
-      (method, endpoint) => {
+      (method, endpoint, args) => {
         server.use(
           rest.get(ENDPOINTS[endpoint], (_req, res, ctx) => {
             return res(ctx.set(HEADERS), ctx.status(500, "My Error"));
@@ -86,17 +94,20 @@ describe("api", () => {
         );
 
         expect.assertions(1);
-        return (api[method]() as Promise<unknown>).catch((err: APIError) => {
-          expect(err.uiErrorMessage).toStrictEqual(
-            'Error from server: "500 My Error". Please send a bug report!'
-          );
-        });
+        // @ts-expect-error -- `args` may have different length/types
+        return (api[method](...args) as Promise<unknown>).catch(
+          (err: APIError) => {
+            expect(err.uiErrorMessage).toStrictEqual(
+              'Error from server: "500 My Error". Please send a bug report!'
+            );
+          }
+        );
       }
     );
 
     test.each(HTTP_GET_ENDPOINTS)(
       ".%s() retries the request",
-      async (method, endpoint, dataKey) => {
+      async (method, endpoint, args, expected) => {
         server.use(
           rest.get(ENDPOINTS[endpoint], (_req, res, ctx) => {
             return res.once(ctx.set(HEADERS), ctx.status(500, "My Error"));
@@ -106,8 +117,9 @@ describe("api", () => {
         expect.assertions(1);
 
         try {
-          const { data } = await api[method]();
-          expect(data).toStrictEqual({ [dataKey]: MOCK_DATA[dataKey] });
+          // @ts-expect-error -- `args` may have different length/types
+          const { data } = await api[method](...args);
+          expect(data).toStrictEqual(expected);
         } catch (err) {
           expect(err).not.toBeDefined();
         }

f322fa7424689e858e470a356f40b7db80dcebdc
Noah Brenner <noah@noahbrenner.com>
2020-12-21 16:30:40 -0800

    Extract useAuth mock into its own module

--- src/components/LoginButton/LoginButton.unit.test.tsx
+++ src/components/LoginButton/LoginButton.unit.test.tsx
@@ -2,44 +2,43 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
 
-import type { Auth } from "#hooks/useAuth";
+import {
+  useAuth,
+  LOADING,
+  ERRORED,
+  UNAUTHENTICATED,
+  AUTHENTICATED,
+} from "#mocks/useAuth";
 import { LoginButton } from "./LoginButton";
 
-let mockUseAuthValue: Partial<Auth>;
-
-jest.mock("#hooks/useAuth", () => ({
-  useAuth: jest.fn(() => mockUseAuthValue),
-}));
-
 describe("<LoginButton />", () => {
-  describe.each(["LOADING", "ERRORED", "UNAUTHENTICATED"] as const)(
-    "given %s state from useAuth()",
-    (authState) => {
-      it('renders a "Log in" button', () => {
-        const login = jest.fn();
-        mockUseAuthValue = { state: authState, login };
-        render(<LoginButton />);
+  describe.each([
+    ["LOADING", LOADING],
+    ["ERRORED", ERRORED],
+    ["UNAUTHENTICATED", UNAUTHENTICATED],
+  ])("given %s state from useAuth()", (_, AUTH_VALUE) => {
+    it('renders a "Log in" button', () => {
+      useAuth.mockReturnValueOnce(AUTH_VALUE);
+      render(<LoginButton />);
 
-        const button = screen.getByRole("button");
-        expect(button).toHaveTextContent(/log ?in/i);
+      const button = screen.getByRole("button");
+      expect(button).toHaveTextContent(/log ?in/i);
 
-        userEvent.click(button);
-        expect(login).toBeCalledTimes(1);
-      });
-    }
-  );
+      userEvent.click(button);
+      expect(AUTH_VALUE.login).toBeCalledTimes(1);
+    });
+  });
 
   describe("given AUTHENTICATED state from useAuth()", () => {
     it('renders a "Log out" button', () => {
-      const logout = jest.fn();
-      mockUseAuthValue = { state: "AUTHENTICATED", logout };
+      useAuth.mockReturnValueOnce(AUTHENTICATED);
       render(<LoginButton />);
 
       const button = screen.getByRole("button");
       expect(button).toHaveTextContent(/log ?out/i);
 
       userEvent.click(button);
-      expect(logout).toBeCalledTimes(1);
+      expect(AUTHENTICATED.logout).toBeCalledTimes(1);
     });
   });
 });

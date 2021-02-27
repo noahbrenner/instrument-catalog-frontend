83f269a3c8e6716c88f8ad25c0942fc8f9be74fa
Noah Brenner <noah@noahbrenner.com>
2021-02-26 16:54:06 -0800

    Mock missing window.scroll in tests/jest.setup.ts
    jsdom doesn't implement this method, so we're mocking it to avoid errors

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -4,3 +4,8 @@ import { server } from "#test_helpers/server";
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers()); // Reset any test-specific handlers
 afterAll(() => server.close());
+
+// Mock functions missing from jsdom for tests that use the jsdom environment
+if (typeof window !== "undefined") {
+  window.scroll = jest.fn();
+}

511291a24a1e5a0ba84c421afbb2c03d92cedffb
Noah Brenner <noah@noahbrenner.com>
2021-03-18 17:36:16 -0700

    Add "New Instrument" link to <Nav /> if logged in
    This commit also moves the definitions for link text and paths to the
    `<Nav />` component. They were previously passed in as a prop.

--- src/components/Nav/Nav.unit.test.tsx
+++ src/components/Nav/Nav.unit.test.tsx
@@ -2,32 +2,60 @@ import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
 
+import {
+  useAuth,
+  mockAuthenticatedUser,
+  LOADING,
+  ERRORED,
+  UNAUTHENTICATED,
+} from "#mocks/useAuth";
 import { renderWithDefaultTheme } from "#test_helpers/renderWithDefaultTheme";
 import { Nav } from "./Nav";
-import type { NavProps } from "./Nav";
 
 const noop = (): void => undefined;
 
 describe("<Nav />", () => {
-  describe("given a list of text and URLs", () => {
-    it("renders a link for each text/URL pair", () => {
-      const inputLinks: NavProps["links"] = [
-        ["foo", "/foo"],
-        ["bar", "/bar"],
-      ];
-      renderWithDefaultTheme(
-        <Nav visible onLinkClick={noop} links={inputLinks} />
+  describe("given a user who is logged in", () => {
+    it("renders all links, including those requiring authentication", () => {
+      mockAuthenticatedUser("foo|123");
+      const { getByRole } = renderWithDefaultTheme(
+        <Nav visible onLinkClick={noop} />
       );
-      const links = screen.getAllByRole("link");
 
-      expect(links).toHaveLength(inputLinks.length);
+      const homeLink = getByRole("link", { name: /home/i });
+      const categoriesLink = getByRole("link", { name: /categories/i });
+      const newInstrumentLink = getByRole("link", { name: /new instrument/i });
 
-      links.forEach((link, index) => {
-        const [linkText, url] = inputLinks[index];
-        expect(link).toHaveTextContent(linkText);
-        expect(link).toHaveAttribute("href", url);
-      });
+      expect(homeLink).toHaveAttribute("href", "/");
+      expect(categoriesLink).toHaveAttribute("href", "/categories/");
+      expect(newInstrumentLink).toHaveAttribute("href", "/instruments/new/");
     });
+  });
+
+  describe("given a user who is not logged in", () => {
+    it.each([
+      ["LOADING", LOADING],
+      ["ERRORED", ERRORED],
+      ["UNAUTHENTICATED", UNAUTHENTICATED],
+    ])(
+      "omits links for pages requiring authentication when auth state is %s",
+      (_, AUTH_VALUE) => {
+        useAuth.mockReturnValue(AUTH_VALUE);
+        const { getByRole, queryByRole } = renderWithDefaultTheme(
+          <Nav visible onLinkClick={noop} />
+        );
+
+        const homeLink = getByRole("link", { name: /home/i });
+        const categoriesLink = getByRole("link", { name: /categories/i });
+        const newInstrumentLink = queryByRole("link", {
+          name: /new instrument/i,
+        });
+
+        expect(homeLink).toHaveAttribute("href", "/");
+        expect(categoriesLink).toHaveAttribute("href", "/categories/");
+        expect(newInstrumentLink).not.toBeInTheDocument();
+      }
+    );
   });
 
   // I'd rather test the `visible` property by the element's visibility instead
@@ -35,7 +63,7 @@ describe("<Nav />", () => {
   // https://github.com/jsdom/jsdom/blob/9b15aee0074870bf18ef3374d5bded9911066125/lib/jsdom/level2/style.js#L18
   describe("given a particular value for visible=...", () => {
     it("is visible when visible=true", () => {
-      renderWithDefaultTheme(<Nav visible onLinkClick={noop} links={[]} />);
+      renderWithDefaultTheme(<Nav visible onLinkClick={noop} />);
       expect(document.body).toHaveClass("nav-visible");
 
       // This would be better
@@ -43,9 +71,7 @@ describe("<Nav />", () => {
     });
 
     it("is hidden when visible=false", () => {
-      renderWithDefaultTheme(
-        <Nav visible={false} onLinkClick={noop} links={[]} />
-      );
+      renderWithDefaultTheme(<Nav visible={false} onLinkClick={noop} />);
       expect(document.body).not.toHaveClass("nav-visible");
 
       // This would be better
@@ -57,18 +83,14 @@ describe("<Nav />", () => {
   describe("given an onLinkClick handler", () => {
     it("calls the click handler for clicks on links", () => {
       const handler = jest.fn();
-      renderWithDefaultTheme(
-        <Nav visible onLinkClick={handler} links={[["foo", "#"]]} />
-      );
-      userEvent.click(screen.getByRole("link"));
+      renderWithDefaultTheme(<Nav visible onLinkClick={handler} />);
+      userEvent.click(screen.getAllByRole("link")[0]);
       expect(handler).toHaveBeenCalledTimes(1);
     });
 
     it("does NOT call the click handler for clicks outside of links", () => {
       const handler = jest.fn();
-      renderWithDefaultTheme(
-        <Nav visible onLinkClick={handler} links={[["foo", "#"]]} />
-      );
+      renderWithDefaultTheme(<Nav visible onLinkClick={handler} />);
       userEvent.click(screen.getByRole("navigation"));
       userEvent.click(screen.getByRole("list"));
       expect(handler).not.toHaveBeenCalled();
